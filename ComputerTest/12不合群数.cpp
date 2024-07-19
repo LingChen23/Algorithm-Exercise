dfb0873c2d263d87b88e73d1d5e846a18d267e1a
yy <1667253917@qq.com>
2024-07-19 13:28:56 +0800

    练习

---
+++ ComputerTest/12不合群数.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 16672 on 2024/7/18.
+//
+//int 2*10^9
+
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+int a, b;
+
+int main(){
+    scanf("%d %d", &a, &b);
+
+
+}
