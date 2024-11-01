89f47cc8e0ffa3bb434e60b94c2125d44e68c4d0
yy <1667253917@qq.com>
2024-11-01 13:05:23 +0800

    第十天：链表

--- LeetcodeBasis/leetcode/editor/cn/leetcode_num_206.cpp
+++ LeetcodeBasis/leetcode/editor/cn/leetcode_num_206.cpp
@@ -64,6 +64,18 @@ namespace solution206{
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        /*
+        if(!head || !head->next){
+            return head;
+        }
+
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return last;
+         */
+
         if(!head || !head->next){
             return head;
         }
