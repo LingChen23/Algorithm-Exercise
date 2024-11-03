50eef3d067525a95303ba635b4894dbc4cb78a78
yy <1667253917@qq.com>
2024-11-03 12:05:45 +0800

    第十二天：二叉树-递归前序遍历

---
+++ LeetcodeBasis/leetcode/editor/cn/leetcode_num_144.cpp
@@ -0,0 +1,107 @@
+//import universal *.h
+#include "../../../stdc.h"
+
+using namespace std;
+
+
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：root = [1,null,2,3] 
+// 
+//
+// 输出：[1,2,3] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：root = [1,2,3,4,5,null,8,null,null,6,7,9] 
+// 
+//
+// 输出：[1,2,4,5,6,7,3,8,9] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 3： 
+//
+// 
+// 输入：root = [] 
+// 
+//
+// 输出：[] 
+//
+// 示例 4： 
+//
+// 
+// 输入：root = [1] 
+// 
+//
+// 输出：[1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1289 👎 0
+
+
+namespace solution144{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void travelsal(TreeNode* cur, vector<int>& vec){
+        if(cur == nullptr){
+            return;
+        }
+
+        vec.push_back(cur->val);
+        travelsal(cur->left, vec);
+        travelsal(cur->right, vec);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        travelsal(root, result);
+
+        return result;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution144;
+int main() {
+    Solution solution = Solution();
+
+    return 0;
+}
