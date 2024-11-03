37eeceb62b49c5cabae979a5a35a4b748063bea2
yy <1667253917@qq.com>
2024-11-03 12:36:15 +0800

    第十二天：二叉树-递归中序遍历

---
+++ LeetcodeBasis/leetcode/editor/cn/leetcode_num_109.cpp
@@ -0,0 +1,78 @@
+//import universal *.h
+#include "../../../stdc.h"
+
+using namespace std;
+
+
+//给定一个单链表的头节点 head ，其中的元素 按升序排序 ，将其转换为 平衡 二叉搜索树。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: head = [-10,-3,0,5,9]
+//输出: [0,-3,9,-10,null,5]
+//解释: 一个可能的答案是[0，-3,9，-10,null,5]，它表示所示的高度平衡的二叉搜索树。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: head = []
+//输出: []
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// head 中的节点数在[0, 2 * 10⁴] 范围内 
+// -10⁵ <= Node.val <= 10⁵ 
+// 
+//
+// Related Topics 树 二叉搜索树 链表 分治 二叉树 👍 919 👎 0
+
+
+namespace solution109{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution109;
+int main() {
+    Solution solution = Solution();
+
+    return 0;
+}
