58d3bc2bffb6aab6ce8d618b134843f2ef31f01e
yy <1667253917@qq.com>
2024-11-05 15:01:53 +0800

    第十四天-字符串-重复的子字符串-移动匹配和KMP算法

--- LeetcodeBasis/leetcode/editor/cn/leetcode_num_459.cpp
+++ LeetcodeBasis/leetcode/editor/cn/leetcode_num_459.cpp
@@ -51,8 +51,54 @@ namespace solution459{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
+
     bool repeatedSubstringPattern(string s) {
-        
+        /*
+        *** 移动匹配 ***
+         *          *
+        string ss = s + s;
+        ss.erase(ss.begin());
+        ss.erase(ss.end() - 1);
+
+        //ss.find(s)：这个调用会在字符串 ss 中搜索子字符串 s。
+        // 如果找到，find 方法会返回 s 在 ss 中首次出现的位置（索引），
+        // 如果没有找到，它会返回 std::string::npos（通常是一个很大的值，表示“未找到”）
+        if(ss.find(s) != std::string::npos )
+            return true;
+        return false;
+         */
+
+        //*** KMP ***
+        //1.求next数组
+        if(s.size() == 0){
+            return false;
+        }
+
+        int next[s.size()];
+        getNext(next, s);
+
+        int len = s.size();
+        if(next[len - 1] != 0 && len % (len - next[s.size() - 1]) == 0)
+            return true;
+        return false;
+
+    }
+
+    void getNext(int* next, const string& s){
+        //1.初始化
+        //2.前后缀不同情况
+        //3.前后缀相同情况
+        next[0] = 0;
+        int j = 0;
+        for(int i = 1; i < s.size(); i++){
+            while(j > 0 && s[i] != s[j]){
+                j = next[j - 1];    //j 等于前一个
+            }
+            if(s[i] == s[j]){
+                j++;
+            }
+            next[i] = j;
+        }
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
