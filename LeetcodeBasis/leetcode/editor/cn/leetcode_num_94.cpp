cc4beb86dd2b56737c6edea0af0d01850b6024f2
yy <1667253917@qq.com>
2024-12-11 21:54:34 +0800

    第19天：二叉树-非递归遍历-中序

--- LeetcodeBasis/leetcode/editor/cn/leetcode_num_94.cpp
+++ LeetcodeBasis/leetcode/editor/cn/leetcode_num_94.cpp
@@ -71,7 +71,20 @@ public:
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
-        travelsal(root, result);
+        stack<TreeNode *> st;
+
+        TreeNode *cur = root;
+        while(cur != nullptr || !st.empty()){
+            if(cur != nullptr){
+                st.push(cur);
+                cur = cur->left;
+            }else{
+                cur = st.top();
+                st.pop();
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
 
         return result;
     }
