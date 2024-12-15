49bd8cd9de84a2321488a261f25683ad90795813
yy <1667253917@qq.com>
2024-12-15 21:37:05 +0800

    第21天：二叉树-二叉树的最大深度-后续

--- LeetcodeBasis/leetcode/editor/cn/leetcode_num_104.cpp
+++ LeetcodeBasis/leetcode/editor/cn/leetcode_num_104.cpp
@@ -56,7 +56,13 @@ namespace solution104{
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        
+        if(root == nullptr){
+            return 0;
+        }
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return max(leftDepth, rightDepth) + 1;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
